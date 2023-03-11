867911fe6c12d655e6498d82a4493f716d5ac7b8
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-11 12:42:17 -0500

    Fin del video

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:http_app/infrastructure/models/pokemon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  Pokemon? pokemon;
+  int pokemonId = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getPokemon();
+  }
+
+  Future<void> getPokemon() async {
+    pokemonId++;
+    final response = await Dio().get('https://pokeapi.co/api/v2/pokemon/$pokemonId');
+    pokemon = Pokemon.fromJson(response.data);
+
+    setState(() {});
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Petición Http'),
+        title: Text('Pokemon ID: $pokemonId'),
       ),
-      body: const SizedBox(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text( pokemon?.name ?? 'No data' ),
+
+            if ( pokemon != null )
+            ...[
+              Image.network(pokemon!.sprites.frontDefault),
+              Image.network(pokemon!.sprites.backDefault),
+            ]
+
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.navigate_next ),
-        onPressed: () {},
+        onPressed: getPokemon,
+        child: const Icon( Icons.navigate_next )
       ),
     );
   }
